ac16de23b832e8338d5984cafc59d5aaf1040fa6
Aditya Vamsi <chvamsiaditya33@gmail.com>
2024-12-09 21:16:51 +0530

    Connected Backend and frontend for register,login routes of user and captain

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -5,7 +5,12 @@ import CaptainLogin from './pages/CaptainLogin';
 import CaptainSignup from './pages/CaptainSignup';
 import UserLogin from './pages/UserLogin';
 import UserSignup from './pages/UserSignup';
-import { UserDataContext } from './context/userContext';
+import Start from './pages/Start';
+import UserProtectWrapper from './pages/UserProtectWrapper';
+import UserLogout from './pages/UserLogout';
+import CaptainHome from './pages/CaptainHome';
+import CaptainProtectWrapper from './pages/CaptainProtectWrapper';
+
 
 const App=()=>{
 
@@ -13,11 +18,20 @@ const App=()=>{
   return (
     <div>
       <Routes>
-        <Route path='/' element={<Home/>}/>
+        <Route path='/' element={<Start/>}/>
         <Route path='/login' element={<UserLogin/>}/>
         <Route path='/signup' element={<UserSignup/>}/>
         <Route path='/captain-login' element={<CaptainLogin/>}/>
         <Route path='/captain-signup' element={<CaptainSignup/>}/>
+        <Route path='/home' 
+          element={<UserProtectWrapper> <Home/> </UserProtectWrapper>}>
+        </Route>
+        <Route path='/user/logout' 
+          element={<UserProtectWrapper> <UserLogout/> </UserProtectWrapper>}>
+        </Route>
+        <Route path='/captain-home' 
+          element={<CaptainProtectWrapper> <CaptainHome/> </CaptainProtectWrapper>}>
+        </Route>
       </Routes>
     </div>
   )
--- Frontend/src/pages/UserSignup.jsx
+++ Frontend/src/pages/UserSignup.jsx
@@ -1,6 +1,7 @@
-import React, { useState } from 'react'
-import { Link } from 'react-router-dom'
-
+import React, { useState, useContext } from 'react'
+import { Link, useNavigate } from 'react-router-dom'
+import axios from 'axios';
+import {UserDataContext} from '../context/userContext';
 const UserSignup = () => {
 
   const [email, setEmail] = useState('');
@@ -9,16 +10,30 @@ const UserSignup = () => {
   const [lastName, setLastName] = useState('');
   const [userData, setUserData] = useState({});
 
-  const submitHandler=(e)=>{
-    e.preventDefault();
-    setUserData({
-      fullName:{
-        firstName:firstName,
-        lastName:lastName
+  const navigate = useNavigate();
+
+  const {user, setUser} = useContext(UserDataContext);
+
+  const submitHandler= async(e)=>{
+    e.preventDefault()
+    const newUser = {
+      fullname:{
+        firstname:firstName,
+        lastname:lastName
       },
       email:email,
       password:password
-    })
+    }
+
+    const response = await axios.post(`${import.meta.env.VITE_BASE_URL}/users/register`,newUser);
+
+    if(response.status===201){
+      const data = response.data
+      setUser(data.user)
+      localStorage.setItem('token',data.token)
+      navigate('/home')
+    }
+
     console.log(userData);
     
     setEmail('');
@@ -32,7 +47,7 @@ const UserSignup = () => {
     <div className='p-7 h-screen flex flex-col justify-between'>
       <div>
         <img className='w-16 mb-10' src="https://upload.wikimedia.org/wikipedia/commons/c/cc/Uber_logo_2018.png" alt="" />
-        <form onSubmit={(e)=>{submitHandler(e)}}>
+        <form onSubmit={(e)=>{submitHandler (e)}}>
 
           <h3 className='text-lg font-medium mb-2'>What's your name</h3>
 
@@ -76,7 +91,7 @@ const UserSignup = () => {
             }}
             />
           <button className='bg-[#111] text-white font-semibold mb-7 rounded px-4 py-2 border w-full text-lg placeholder:text-base' 
-          >Login</button>
+          >Create account</button>
         </form>
         <p className='text-center'>Already have a account?
           <Link to='/login' className='text-blue-600'>Login here</Link>
